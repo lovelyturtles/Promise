9acd7fab131504ba92656ffb6999ed96342079db
Siwen Sun <51042404+SunSiwen@users.noreply.github.com>
2022-02-14 10:01:13 -0600

    build classes

---
+++ app/src/main/java/comp3350/group6/promise/presentation/MainActivity.java
@@ -0,0 +1,15 @@
+package comp3350.group6.promise.application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comp3350.group6.promise.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
