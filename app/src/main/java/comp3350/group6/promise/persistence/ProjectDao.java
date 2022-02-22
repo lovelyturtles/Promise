7379e1d763e5eb8ae6cb3009b654e74d8ea9ea75
Dani Youn <semi159753@naver.com>
2022-02-22 15:47:21 -0600

    cherry picking

---
+++ app/src/main/java/comp3350/group6/promise/persistence/ProjectDao.java
@@ -0,0 +1,16 @@
+package comp3350.group6.promise.persistence;
+
+import java.util.List;
+
+import comp3350.group6.promise.objects.Project;
+
+public interface ProjectDao {
+
+    List<Project> getProjectList();
+
+    Project insertProject(Project project);
+
+    Project updateProject(Project project);
+
+    void deleteProject(Project project);
+}
