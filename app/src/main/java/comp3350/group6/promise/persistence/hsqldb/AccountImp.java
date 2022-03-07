a0b6b332006fae73b25c2c26b2ffa673ca4d37fe
Siwen Sun <51042404+SunSiwen@users.noreply.github.com>
2022-03-07 01:36:23 -0600

    local db done

--- app/src/main/java/comp3350/group6/promise/persistence/hsqldb/AccountImp.java
+++ app/src/main/java/comp3350/group6/promise/persistence/hsqldb/AccountImp.java
@@ -12,6 +12,7 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import comp3350.group6.promise.objects.Account;
 import comp3350.group6.promise.persistence.AccountDao;
@@ -20,7 +21,7 @@ import comp3350.group6.promise.util.DBConnectorUtil;
 public class AccountImp implements AccountDao {
 
     @Override
-    public int createAccount(String email, String password, int userId) throws Exception{
+    public int createAccount(String email, String password, int userId) throws Exception {
         password = new String(Base64.encodeBase64URLSafe(password.getBytes(StandardCharsets.UTF_8)), Charset.defaultCharset());
         Connection cnn = DBConnectorUtil.getConnection();
         assert cnn != null;
@@ -31,43 +32,70 @@ public class AccountImp implements AccountDao {
         try {
             preparedStatement.executeUpdate();
         } catch (Exception e) {
-            throw e;
+            e.printStackTrace();
         }
         return userId;
     }
 
     @Override
-    public int changePassword(int userId, String oldPassword, String newPassword) throws Exception{
+    public int changePassword(int userId, String oldPassword, String newPassword) throws Exception {
         oldPassword = new String(Base64.encodeBase64URLSafe(oldPassword.getBytes(StandardCharsets.UTF_8)), Charset.defaultCharset());
         newPassword = new String(Base64.encodeBase64URLSafe(newPassword.getBytes(StandardCharsets.UTF_8)), Charset.defaultCharset());
         Connection cnn = DBConnectorUtil.getConnection();
-        int res = 0;
+        assert cnn != null;
+        PreparedStatement preparedStatement = cnn.prepareStatement("update Account set password = ? where userId = ? and password = ?");
+        preparedStatement.setString(1, newPassword);
+        preparedStatement.setInt(2, userId);
+        preparedStatement.setString(3, oldPassword);
+        return  preparedStatement.executeUpdate();
+    }
+
+
+    @Override
+    public boolean accountExists(String email) {
+        Connection cnn = DBConnectorUtil.getConnection();
         try {
             assert cnn != null;
-            PreparedStatement preparedStatement = cnn.prepareStatement("update Account set password = ? where userId = ? and password = ?");
-            preparedStatement.setString(1, newPassword);
-            preparedStatement.setInt(2, userId);
-            preparedStatement.setString(3, oldPassword);
-            res = preparedStatement.executeUpdate();
+            PreparedStatement preparedStatement = cnn.prepareStatement("select * from account where email = ?");
+            preparedStatement.setString(1, email);
+            ResultSet res = preparedStatement.executeQuery();
+            return res.next();
         } catch (Exception e) {
-            throw e;
+            e.printStackTrace();
         }
-        return res;
-    }
-
-
-    @Override
-    public boolean accountExists( String email ){
-        return false; //this method not implemented for database yet
+        return false;
     }
 
     @Override
-    public Account getAccountByEmail(String email ){
+    public Account getAccountByEmail(String email) {
+        Connection cnn = DBConnectorUtil.getConnection();
+        try {
+            assert cnn != null;
+            PreparedStatement preparedStatement = cnn.prepareStatement("select * from account where email = ?");
+            preparedStatement.setString(1, email);
+            ResultSet res = preparedStatement.executeQuery();
+            res.next();
+            return new Account(res.getString("email"), null, res.getInt("userId"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
-    public boolean passwordsMatch( String email, String password ){
-        return false; //this method not implemented for database yet
+    public boolean passwordsMatch(String email, String password) {
+        password = new String(Base64.encodeBase64URLSafe(password.getBytes(StandardCharsets.UTF_8)), Charset.defaultCharset());
+        Connection cnn = DBConnectorUtil.getConnection();
+        try {
+            assert cnn != null;
+            PreparedStatement preparedStatement = cnn.prepareStatement("select * from account where email = ? and password = ?");
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet.next();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
