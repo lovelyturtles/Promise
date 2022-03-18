725216c1cef17221316d81238aed8a69aad35044
Tung Nguyen <tungnguyen@MacBook-Pro.hitronhub.home>
2022-03-18 01:20:51 -0500

    add Integration test class

--- app/src/test/java/comp3350/group6/promise/tests/IntegrationTests.java
+++ app/src/test/java/comp3350/group6/promise/tests/IntegrationTests.java
@@ -1,5 +1,16 @@
 package comp3350.group6.promise.tests;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import comp3350.group6.promise.tests.business.HandleIT;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        HandleIT.class
+})
+
+
 public class IntegrationTests {
 
 }
---
+++ app/src/test/java/comp3350/group6/promise/tests/business/HandleIT.java
@@ -0,0 +1,11 @@
+package comp3350.group6.promise.tests.business;
+
+import java.io.File;
+
+import comp3350.group6.promise.objects.Handle;
+
+public class HandleIT {
+    private Handle handle;
+    private File testDB;
+
+}
