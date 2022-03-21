80178385a9e633f44e9b25a462127ef24e558870
Siwen Sun <51042404+SunSiwen@users.noreply.github.com>
2022-03-21 10:16:02 -0500

    handle exception and return null object

--- app/src/main/java/comp3350/group6/promise/persistence/hsqldb/UserImp.java
+++ app/src/main/java/comp3350/group6/promise/persistence/hsqldb/UserImp.java
@@ -9,7 +9,6 @@ package comp3350.group6.promise.persistence.hsqldb;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 import comp3350.group6.promise.objects.User;
@@ -19,9 +18,9 @@ import comp3350.group6.promise.util.DBConnectorUtil;
 public class UserImp implements UserDao {
 
     @Override
-    public int addUser(String name, String introduction) throws Exception {
+    public int addUser(String name, String introduction) {
         try (final Connection cnn = DBConnectorUtil.getConnection()) {
-
+            assert cnn != null;
             PreparedStatement preparedStatement = cnn.prepareStatement("insert into User (name,introduction) values (?,?)", Statement.RETURN_GENERATED_KEYS);
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, introduction);
@@ -29,38 +28,45 @@ public class UserImp implements UserDao {
             ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
             generatedKeys.next();
             return generatedKeys.getInt(1);
+        }catch (Exception e){
+            e.printStackTrace();
         }
+        return -1;
     }
 
     @Override
-    public void updateUserByUserId(int userId, String name, String introduction) throws SQLException {
+    public int updateUserByUserId(int userId, String name, String introduction) {
         try (final Connection cnn = DBConnectorUtil.getConnection()) {
-
+            assert cnn != null;
             PreparedStatement preparedStatement = cnn.prepareStatement("update User set name = ? introduction = ? where userId = ?");
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, introduction);
             preparedStatement.setInt(3, userId);
             preparedStatement.executeUpdate();
-
+            return 1;
+        }catch (Exception e){
+            e.printStackTrace();
         }
+        return -1;
     }
 
     @Override
-    public User getUserByUserId(int userId) throws Exception {
+    public User getUserByUserId(int userId) {
 
         try (final Connection cnn = DBConnectorUtil.getConnection()) {
-
+            assert cnn != null;
             PreparedStatement preparedStatement = cnn.prepareStatement("select userId, name, introduction where userId = ?");
             preparedStatement.setInt(1, userId);
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) { //每进行一次next()，就将遍历一行属性的值
-                int id = resultSet.getInt("userId"); //获取第一列（id)的数据
-                String name = resultSet.getString("name"); //获取第二列（bookName）的数据
+            if (resultSet.next()) {
+                int id = resultSet.getInt("userId");
+                String name = resultSet.getString("name");
                 String introduction = resultSet.getString("introduction");
                 return new User(id, name, introduction);
             }
-
-            return new User(userId, null, null);
+        }catch (Exception e){
+            e.printStackTrace();
         }
+        return new User(userId, null, null);
     }
 }
