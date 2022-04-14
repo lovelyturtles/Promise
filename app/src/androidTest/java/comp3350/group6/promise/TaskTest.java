c5abcb8a68c93d5eff9a93fb25b0ad83b3588eb0
Tung Nguyen <nguye49@myumanitoba.ca>
2022-04-14 17:25:02 -0500

    edit UI test

--- app/src/androidTest/java/comp3350/group6/promise/TaskTest.java
+++ app/src/androidTest/java/comp3350/group6/promise/TaskTest.java
@@ -1,24 +1,46 @@
 package comp3350.group6.promise;
 
 
-import static androidx.test.espresso.Espresso.onView;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@LargeTest
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+
+
+import com.android21buttons.fragmenttestrule.FragmentTestRule;
+
+import comp3350.group6.promise.presentation.MainActivity;
+import comp3350.group6.promise.presentation.Project.ProjectFragment;
+import comp3350.group6.promise.presentation.Task.TaskFragment;
+
+//@LargeTest
 @RunWith(AndroidJUnit4.class)
 public class TaskTest {
+
 //    @Rule
-//    public ActivityTestRule
+//    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+//
+//    @Test
+//    public void createTask() {
+//        onView(withId(R.id.registerLink)).perform(click());
+//    }
+
+
+    @Rule
+    public FragmentTestRule<?, TaskFragment> fragmentTestRule = FragmentTestRule.create(TaskFragment.class);
+
 
     @Test
-    public void createTask() {
-//        onView(withId)
+    public void createTask(){
+        onView(withId(R.id.toolbar)).perform(click());
     }
 }
